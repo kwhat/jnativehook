7c220a803726e5f26b6deab41901e3ef5d506064
Alexander Barker <alex@1stleg.com>
2007-10-17 06:55:12 +0000

    update linux test code to work using the xevie libs.

--- src/org/dotnative/globalhook/keyboard/cpp_source/Linux/x86_32/start.sh
+++ src/org/dotnative/globalhook/keyboard/cpp_source/Linux/x86_32/start.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-g++ -m32 -o test.bin -march=i586 -lX11 ./test.cpp
+g++ -m32 -o test.bin -march=i586 -lX11 -lXevie ./test.cpp
 
 ./test.bin &
 PID=$!
--- src/org/dotnative/globalhook/keyboard/cpp_source/Linux/x86_32/test.cpp
+++ src/org/dotnative/globalhook/keyboard/cpp_source/Linux/x86_32/test.cpp
@@ -1,11 +1,41 @@
+//Test CPP.
+// Use XEvie
+/*
+emerge -av libXevie
+
+Section "Extensions"
+    ...
+    Option         "XEVIE" "Enable"
+    ...
+EndSection
+
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <X11/Xlib.h>
+#include <X11/Xproto.h>
+#include <X11/X.h>
+#include <X11/extensions/Xevie.h>
+#include <X11/Xutil.h>
+#include <X11/XKBlib.h>
+
+#include <X11/Intrinsic.h>
+//#include <X11/StringDefs.h>
+//#include <X11/Shell.h>
+
+/*
 #include <stdio.h>
 #include <X11/X.h>
 #include <X11/Xlib.h>
 #include <X11/XKBlib.h>
+#include <X11/Xutil.h>
 #include <X11/Intrinsic.h>
 #include <X11/StringDefs.h>
-#include <X11/Xutil.h>
 #include <X11/Shell.h>
+#include <X11/extensions/Xevie.h>
+*/
 
 char *TranslateKeyCode(XEvent *ev);
 Display  *disp;
@@ -15,11 +45,6 @@ int iKeyCode;
 int iKeyState;
 int iKeyTime;
 
-void snoop_x() {
-	//AnyKey
-	//AnyModifier
-	//XGrabKey(disp, XKeysymToKeycode(disp, XStringToKeysym("b")), None, DefaultRootWindow(disp), TRUE, GrabModeSync, GrabModeSync); 
-}
 
 int main(int argc, char* argv[]) {
 	//fprintf(stderr, "Loading JNI Keyboard Driver\n");
@@ -38,32 +63,42 @@ int main(int argc, char* argv[]) {
 	//XSetErrorHandler(XErrIgnore);
 	//XAutoRepeatOff(disp);
 	
+	if(XevieStart(disp)) {
+		printf("XevieStart(dpy) finished \n");
+	}
+	else {
+		printf("XevieStart(dpy) failed, only one client is allowed to do event interception\n");
+		exit(1);
+	}
+	
+	
+	XevieSelectInput(disp, KeyPressMask | KeyReleaseMask);
 	XkbSetDetectableAutoRepeat(disp, TRUE, NULL);
-	snoop_x();
 	
 	while (TRUE) {
-		XGrabKeyboard(disp, DefaultRootWindow(disp), TRUE, GrabModeAsync, GrabModeAsync, CurrentTime);
 		XNextEvent(disp, &xev);
+		
 		iKeyCode = xev.xkey.keycode;
 		//iKeyCode = XLookupKeysym(&xev.xkey, xev.xkey.state);
 		iKeyState = xev.xkey.state;
 		iKeyTime = xev.xkey.time;
 		int iKeyType = xev.type;
 		Window tmp = xev.xkey.window;
-		XUngrabKeyboard(disp, CurrentTime);
+		
+		//
+		
+		//XAllowEvents(disp, ReplayKeyboard, CurrentTime);
 		
 		
+		//XPutBackEvent(disp, &xev);
+		//XSelectInput(disp, tmp, KeyPressMask | KeyReleaseMask);
+		//XGrabKey(disp, iKeyCode, None, tmp, TRUE, GrabModeAsync, GrabModeAsync);
+		//XNextEvent(disp, &xev);
 		
-		XPutBackEvent(disp, &xev);
-		XSelectInput(disp, tmp, KeyPressMask | KeyReleaseMask);
-		XGrabKey(disp, iKeyCode, None, tmp, TRUE, GrabModeAsync, GrabModeAsync);
-		XNextEvent(disp, &xev);
+		//XSendEvent(disp, tmp, FALSE, xev.type, &xev); 
+		//XSync(disp, FALSE);
+		//XFlush(disp);
 		
-		//XSendEvent(disp, InputFocus, FALSE, xev.type, &xev); 
-		XAllowEvents(disp, ReplayKeyboard, iKeyTime);
-		
-		
-
 		switch (iKeyType) {
 			case KeyPress:
 				//bKeyDown = TRUE;
@@ -78,15 +113,8 @@ int main(int argc, char* argv[]) {
 		
 		fprintf(stderr, "Key: %i %i %i\n", iKeyCode, xev.type, iKeyTime);
 
-
-		
-		//XSetInputFocus(disp, PointerRoot, RevertToParent, iKeyTime);
-		//XSendEvent(disp, xev.xkey.subwindow, FALSE, xev.type, &xev); 
-
-		XSync(disp, FALSE);
-		XFlush(disp);
-
-		
-		//XUngrabKey(disp, xev.xkey.keycode, None, DefaultRootWindow(disp));
+		XevieSendEvent(disp, &xev, XEVIE_UNMODIFIED);
 	}
+	
+	XevieEnd(disp);
 }
