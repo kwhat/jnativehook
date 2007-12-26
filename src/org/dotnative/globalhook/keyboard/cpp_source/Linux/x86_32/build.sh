05a895fb3d587081d42ee6ccb29667a91473b253
Alexander Barker <alex@1stleg.com>
2007-12-26 02:28:55 +0000

    Reorginized for linux 32 and 64 bit distropution.

--- src/org/dotnative/globalhook/keyboard/cpp_source/Linux/x86_32/build.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-g++ -m32 -march=i586 -shared -fPIC -lX11 -lXevie -I${JAVA_HOME}/include -I${JAVA_HOME}/include/linux ./org_dotnative_globalhook_keyboard_GlobalKeyHook.cpp -o libGlobalKeyListener.so
