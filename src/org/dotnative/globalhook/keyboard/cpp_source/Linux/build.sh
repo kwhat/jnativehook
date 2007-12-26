d29f2ad9c39b70b177581c2abf4455aa5872aadc
Alexander Barker <alex@1stleg.com>
2007-12-26 07:01:01 +0000

    Removed obsolete linux files

--- src/org/dotnative/globalhook/keyboard/cpp_source/Linux/build.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-g++ -m32 -march=i586 -shared -fPIC -lX11 -lXevie -I${JAVA_HOME}/include -I${JAVA_HOME}/include/linux ./org_dotnative_globalhook_keyboard_GlobalKeyHook.cpp -o ./x86_32/libGlobalKeyListener.so
-g++ -m64 -march=k8 -shared -fPIC -lX11 -lXevie -I${JAVA_HOME}/include -I${JAVA_HOME}/include/linux ./org_dotnative_globalhook_keyboard_GlobalKeyHook.cpp -o ./x86_64/libGlobalKeyListener.so
