c59f61a3f7cac66db27fa11f69354208e6e7bd11
Alexander Barker <alex@1stleg.com>
2011-11-06 01:53:53 +0000

    Adding some java based test code.

---
+++ java/DesktopPropties.java
@@ -0,0 +1,24 @@
+/* JNativeHook: Global keyboard and mouse hooking for Java.
+ * Copyright (C) 2006-2011 Alexander Barker.  All Rights Received.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+import java.awt.Toolkit;
+
+public class DesktopPropties {
+	public static void main(String[] args) {
+		System.out.println(Toolkit.getDefaultToolkit().getDesktopProperty("awt.multiClickInterval"));
+	}
+}
