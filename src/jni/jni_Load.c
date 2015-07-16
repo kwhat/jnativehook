7f84207d23c671f56ec246e738fe31228d148ea9
Alexander Barker <alex@1stleg.com>
2015-07-15 22:58:58 -0700

    Fixed default thread name. Removed LGPL on start.

--- src/jni/jni_Load.c
+++ src/jni/jni_Load.c
@@ -29,7 +29,7 @@
 JavaVM *jvm;
 JavaVMAttachArgs jvm_attach_args = {
 	.version = JNI_VERSION_1_4,
-	.name = "JNativeHook Native Hook",
+	.name = "JNativeHook Hook Thread",
 	.group = NULL
 };
 
@@ -43,38 +43,6 @@ JNIEXPORT jint JNICALL JNI_OnLoad(JavaVM *vm, void *reserved) {
 		// Create all the global class references onload to prevent class loader
 		// issues with JNLP and some IDE's.
 		if (jni_CreateGlobals(env) == JNI_OK) {
-			#ifndef USE_QUIET
-			jclass PrintStream_class = (*env)->FindClass(env, "java/io/PrintStream");
-			jfieldID out_id = (*env)->GetStaticFieldID(env, java_lang_System->cls, "out", "Ljava/io/PrintStream;");
-
-			if (PrintStream_class != NULL && out_id != NULL) {
-				jmethodID println_id = (*env)->GetMethodID(env, PrintStream_class, "println", "(Ljava/lang/String;)V");
-				jobject out = (*env)->GetStaticObjectField(env, PrintStream_class, out_id);
-
-				if (println_id != NULL && out != NULL) {
-					jstring copyright = (*env)->NewStringUTF(env, "\n"
-							"JNativeHook: Global keyboard and mouse hooking for Java.\n"
-							"Copyright (C) 2006-2015 Alexander Barker.  All Rights Received.\n"
-							"https://github.com/kwhat/jnativehook/\n"
-							"\n"
-							"JNativeHook is free software: you can redistribute it and/or modify\n"
-							"it under the terms of the GNU Lesser General Public License as published\n"
-							"by the Free Software Foundation, either version 3 of the License, or\n"
-							"(at your option) any later version.\n"
-							"\n"
-							"JNativeHook is distributed in the hope that it will be useful,\n"
-							"but WITHOUT ANY WARRANTY; without even the implied warranty of\n"
-							"MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the\n"
-							"GNU General Public License for more details.\n"
-							"\n"
-							"You should have received a copy of the GNU Lesser General Public License\n"
-							"along with this program.  If not, see <http://www.gnu.org/licenses/>.\n");
-
-					(*env)->CallVoidMethod(env, out, println_id, copyright);
-				}
-			}
-			#endif
-
 			// Set Java logger for native code messages.
 			hook_set_logger_proc(&uiohook_LoggerCallback);
 
