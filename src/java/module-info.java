fd7ba6e0c721f1773b129d7695500a5628bda40b
Degubi <gergonoorbi@gmail.com>
2019-02-08 23:20:48 +0100

    Add module-info file

---
+++ src/java/module-info.java
@@ -0,0 +1,9 @@
+module org.jnativehook {
+	exports org.jnativehook;
+	exports org.jnativehook.dispatcher;
+	exports org.jnativehook.keyboard;
+	exports org.jnativehook.mouse;
+
+	requires java.desktop;
+	requires java.logging;
+}
